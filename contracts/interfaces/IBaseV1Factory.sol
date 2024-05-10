c8bffa2c9b7fd617e3a7431e67505ec1f204c934
JokerTheBond <simor98@gmail.com>
2024-05-10 12:00:35 +0200

    getPool() for Aerodrome

--- contracts/interfaces/IBaseV1Factory.sol
+++ contracts/interfaces/IBaseV1Factory.sol
@@ -2,4 +2,5 @@ pragma solidity >=0.5.0;
 
 interface IBaseV1Factory {
     function getPair(address tokenA, address tokenB, bool stable) external view returns (address pair);
+    function getPool(address tokenA, address tokenB, bool stable) external view returns (address pair);
 }
